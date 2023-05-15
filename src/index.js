75c413d23510c4a01f8d7271d16e4ae4ebd1dca1
Madison Newby <madison1999@hotmail.co.uk>
2023-05-15 11:52:27 +0100

    add forecast template

--- src/index.js
+++ src/index.js
@@ -180,6 +180,12 @@ form.addEventListener("submit", showNewWeather);
 form.addEventListener("submit", showNewWeatherImperial);
 form.addEventListener("submit", showNewIcon);
 
+function getForecast(coordinates) {
+  let apiKey = `b1a8336ff1e05b64da5625e4158fbea3`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 //weather and geolocation api work
 function showWeather(response) {
   //city and time
@@ -216,6 +222,8 @@ function showWeather(response) {
   document.querySelector(".sunset").innerHTML = formatTime(
     response.data.sys.sunset * 1000
   );
+
+  getForecast(response.data.coords);
 }
 
 function showWeatherImperial(response) {
@@ -231,6 +239,49 @@ function showIcon(response) {
   document
     .querySelector("#icon")
     .setAttribute("alt", response.data.condition.description);
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="row">
+        <div class="col-2">
+          <p class="five-day-details">${day}</p>
+          <p class="five-day-details">3/3</p>
+        </div>
+        <div class="col-2 weather-image-container">
+          <img src="images/mostly-sunny.png" alt="Mostly sunny icon" />
+        </div>
+        <div class="col-2">
+          <p class="five-day-details">21°C</p>
+          <p class="five-day-details">High</p>
+        </div>
+        <div class="col-2">
+          <p class="five-day-details">10°C</p>
+          <p class="five-day-details">Low</p>
+        </div>
+        <div class="col-2">
+          <p class="five-day-details">12mph</p>
+          <p class="five-day-details">Wind</p>
+        </div>
+        <div class="col-2">
+          <p class="five-day-details">0%</p>
+          <p class="five-day-details">Rain</p>
+        </div>
+      </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function onPositionRetrieveSuccessfully(position, event) {
@@ -322,3 +373,5 @@ document
 document
   .querySelector("#celsius-link")
   .addEventListener("click", convertToCelsius);
+
+displayForecast();
